724b7ea171152e7614363ae89d16260bbe6b0c50
woshuhua <wongxuhuan@gmail.com>
2023-03-31 09:06:32 +0800

    login function system

---
+++ index.js
@@ -0,0 +1,47 @@
+let dbUsers = [
+    {
+        username: "Wong",
+        password: "password",
+        name: "Wong Xu Huan",
+        email: "B022110214@student.utem.edu.my"
+    },
+    {
+        username: "Chua",
+        password: "010uehs",
+        name: "CHua CF",
+        email: "Chua233@gmail.com"
+    },
+    {
+        username: "Lee",
+        password: "18sh38",
+        name: "Lee WH",
+        email: "Lee823@gmail.com"
+    },
+    {
+        username: "Teow",
+        password: "Teow1943",
+        name: "Teow CY",
+        email: "Teow1937@gmail.com"
+    }
+]
+
+function login(username, password) {
+    console.log("Someone try to login with", username, password)
+    let matched = dbUsers.find(element => 
+        element.username == username
+    )
+    if (matched){
+        if(matched.password == password) {
+            return matched
+        } else{
+            return "Password not matched"
+        }
+    } else{
+        return "Username not found"
+    }
+}
+
+//try to login
+console.log(login("Wong", "password"))
+console.log(login("Chua", "123456"))
+console.log(login("utem","1he82"))
